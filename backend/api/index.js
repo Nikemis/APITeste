56071b506aad446a6a602500b0c0e3e6fe865ae4
Aline Nishida <keminishida@gmail.com>
2025-04-27 12:03:16 -0300

    Atualização FrontEnd

--- backend/api/index.js
+++ backend/api/index.js
@@ -6,6 +6,16 @@ import mongoose from 'mongoose'
 import tarefaModel from './models/tarefas.js'
 const app = express();
 
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader('Access-Control-Allow-Methods', 'HEAD, GET, POST, PATCH, DELETE');
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+});
+
 app.use(express.json());
 
 await mongoose.connect(process.env.MONGO_URL);
